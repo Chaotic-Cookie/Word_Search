b129977bcaea91980e35af3b55d06d08358b6fbf
mangamariam <mangamariam2001@gmail.com>
2020-05-26 18:10:47 -0700

    The start of it all

--- src/WordSearchMain.java
+++ src/WordSearchMain.java
@@ -5,7 +5,7 @@ public class WordSearchMain {
     public static Scanner book = new Scanner(System.in);
 
     public static void main(String[] args){
-
+        WordSearch pen = new WordSearch();
         int menu = 0;
 
         while(menu != 4) {
@@ -25,6 +25,7 @@ public class WordSearchMain {
                 case 1:
                     System.out.println("How many words would you like to look for? ");
                     String numWords = book.nextLine();
+                    pen.generate();
                     break;
                 case 2:
                         System.out.println("Printing Word search.");
@@ -32,7 +33,7 @@ public class WordSearchMain {
                     break;
                 case 3:
                     System.out.println("Printing Solutions.");
-                    print();
+                    pen.print();
                     break;
                 case 4:
                     System.out.println("Thank you for using Word Search.");
