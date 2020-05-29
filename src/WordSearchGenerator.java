578868657cbe6765aec8aef6a2e52077a130b535
mangamariam <mangamariam2001@gmail.com>
2020-05-28 19:47:16 -0700

    i have no idea what im doing

--- src/WordSearchGenerator.java
+++ src/WordSearchGenerator.java
@@ -6,12 +6,11 @@ public class WordSearchGenerator {
     private char board [][];
     private char length; //word length
     private int numWords;
-    private WordSearch search;
+    char SIZE = 50;
     private ArrayList<Character> wordList = new ArrayList<Character>();
 
 
     public WordSearchGenerator(int numWords, char length){
-        search = new WordSearch();
         this.length = length;
         this.numWords = numWords;
 
@@ -47,13 +46,6 @@ public class WordSearchGenerator {
     }
 
 
-    public class WordSearch {
-        char SIZE = 50;
-        private WordSearchGenerator start;
-        public WordSearch(){ //creates an empty node
-            start = null;
-        }
-
         public void print(){
 
 
@@ -78,22 +70,7 @@ public class WordSearchGenerator {
 
         ///public void solution(){
         public class solution{
-            //find way to get it to show random letters as X
-            //adapt this to work?
-            //Also a neat thing i found
-            public boolean exist(char[][] board, String word){
 
-                char[] chars = word.toCharArray();
-                for(int i = 0; i < board.length; i++){
-                    for(int j = 0; j < board[0].length; j++){
-                        if(exist(board, i, j, chars, 0)){
-                            return true;
-                        }
-                    }
-                }
-                return false;
-
-            }
 
         }//end solution
 
@@ -110,18 +87,6 @@ public class WordSearchGenerator {
             return wordGen;
         }
 
-    /* neat thing that can help in the future
-    * private void fillUpEmptyTiles() {
-		for (int x = 0; x < fieldRows; x++){
-			for (int y = 0; y < fieldColums; y++){
-				if (tField.getTileFieldObject(x, y).getCharacterToDraw() == null){
-					tField.getTileFieldObject(x, y).setCharacterToDraw("" + randomChars.charAt(Random.nextInt(randomChars.length())));
-				}
-			}
-		}
-	}
-    * */
-    }//end class
 
+}//end class
 
-}// end class
