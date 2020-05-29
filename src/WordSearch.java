c0fcfa330cacfe2d49ad2beb93e1ee70a4ad3e5a
mangamariam <mangamariam2001@gmail.com>
2020-05-28 17:05:19 -0700

    i have no idea what im doing

--- src/WordSearch.java
+++ src/WordSearch.java
@@ -1,5 +1,6 @@
 public class WordSearch {
     int SIZE = 100;
+    private WordSearchGenerator start;
     public WordSearch(){ //creates an empty node
         start = null;
     }
@@ -8,12 +9,23 @@ public class WordSearch {
         String[] randLetters = {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M",  "N", "O", "P", "Q",
                 "R", "S", "T", "U", "V", "W", "X", "Y", "Z"};
 
+        WordSearchGenerator current = null;
         if(start == null){
-            System.out.println("No words to print.");
+            System.out.println("This Word search is empty.");
 
         }else{
+            current = start;
+            System.out.println(current.toString());
+
+            while(current.next != null){
+
+                current = current.next;
+                System.out.println(current.toString());
+
+            }
 
         }
+        System.out.println();
     }//end print
 
     public void solution(){
@@ -26,6 +38,8 @@ public class WordSearch {
         while(numWord > SIZE){
 
         }
+
+        return wordGen;
     }
 }//end class
 
