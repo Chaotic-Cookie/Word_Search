fb4d7fe3a78613552bf8eaf0213e92b018e72f46
mangamariam <mangamariam2001@gmail.com>
2020-05-29 19:28:26 -0700

    i have no idea what im doing

--- src/WordSearchGenerator.java
+++ src/WordSearchGenerator.java
@@ -85,6 +85,112 @@ public class WordSearchGenerator {
             return board;
         }//end solution
 
+    public static int[] findHorizontal(char[][] board, String insert){
+
+        int[] place = new int[2];
+        Random random = new Random();
+        for (int k = 0; k < 100; k++){
+            int hPos = random.nextInt(board.length);
+            int lPos = random.nextInt(board.length - insert.length());
+            for(int i = 0; i < insert.length(); i++){
+                if((board[hPos][lPos +i]!= (insert.charAt(i)) && (board[hPos][lPos +i] != 'X'))){
+                    break;
+                }
+                if(i == insert.length() - 1){
+                    place[0] = hPos;
+                    place[1] = lPos;
+                    return place;
+
+                }
+            }
+
+        }
+        return new int[]{-1, -1};
+
+    }
+    public static int[] findVertical(char[][] board, String insert){
+
+        int[] place = new int[2];
+        Random random = new Random();
+        for (int k = 0; k < 100; k++){
+            int hPos = random.nextInt(board.length- insert.length());
+            int lPos = random.nextInt(board.length);
+            for(int i = 0; i < insert.length(); i++){
+                if((board[hPos][lPos +i]!= (insert.charAt(i)) && (board[hPos +i][lPos] != 'X'))){
+                    break;
+                }
+                if(i == insert.length() - 1){
+                    place[1] = hPos;
+                    place[0] = lPos;
+                    return place;
+
+                }
+            }
+
+        }
+        return new int[]{-1, -1};
+
+    }
+    public static int[] findAngle(char[][] board, String insert){
+
+        int[] place = new int[2];
+        Random random = new Random();
+        for (int k = 0; k < 100; k++){
+            int hPos = random.nextInt(board.length - insert.length());
+            int lPos = random.nextInt(board.length - insert.length());
+            for(int i = 0; i < insert.length(); i++){
+                if((board[hPos][lPos +i]!= (insert.charAt(i)) && (board[hPos +i][lPos +i] != 'X'))){
+                    break;
+                }
+                if(i == insert.length() - 1){
+                    place[1] = hPos;
+                    place[1] = lPos;
+                    return place;
+
+                }
+            }
+
+        }
+        return new int[]{-1, -1};
+
+    }
+    public static char[][] doHorizontal(int[] position, String insert, char[][] board){
+        // Places an array of characters horizontally into the board array.
+
+        int hPos = position[0]; // Vertical Position
+        int wordLength = insert.length();
+        int lPos = position[1]; //Horizontal Position
+
+        for (int wordChar = 0; wordChar < wordLength; wordChar++){
+            board[hPos][lPos + wordChar] = insert.charAt(wordChar);
+        }
+        return board;
+    }
+    public static char[][] doVertical(int[] position, String insert, char[][] board){
+        // Places an array of characters Vertical into the board array.
+
+        int hPos = position[1]; // Vertical Position
+        int wordLength = insert.length();
+        int lPos = position[0]; //Horizontal Position
+
+        for (int wordChar = 0; wordChar < wordLength; wordChar++){
+            board[hPos + wordChar][lPos] = insert.charAt(wordChar);
+        }
+        return board;
+    }
+    public static char[][] doAngle(int[] position, String insert, char[][] board){
+        // Places an array of characters Angular into the board array.
+
+        int hPos = position[1]; // Vertical Position
+        int wordLength = insert.length();
+        int lPos = position[1]; //Horizontal Position
+
+        for (int wordChar = 0; wordChar < wordLength; wordChar++){
+            board[hPos+ wordChar][lPos + wordChar] = insert.charAt(wordChar);
+        }
+        return board;
+    }
+
     /*public void wordGenerate() {
 
 
